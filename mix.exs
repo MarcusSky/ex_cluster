cf8dc4249c11e992232becadaf06780a5e4b2dcd
Marcus Vieira <marcusbrunovieira@gmail.com>
2019-07-30 20:12:07 -0300

    configure mix release

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule ExCluster.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        ex_cluster: [
+          include_executables_for: [:unix],
+          applications: [runtime_tools: :permanent]
+        ]
+      ]
     ]
   end
 
