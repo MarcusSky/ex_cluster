05db9b118386add6176582017a284359cb030174
Marcus Vieira <marcusbrunovieira@gmail.com>
2019-07-31 13:23:09 -0300

    add `lib_cluster`

--- lib/ex_cluster.ex
+++ lib/ex_cluster.ex
@@ -6,56 +6,22 @@ defmodule ExCluster do
   def start(_type, _args) do
     Logger.info("ExCluster application started")
 
-    case System.get_env("NODES") do
-      nodes when is_binary(nodes) ->
-        nodes
-        # convert list of nodes into atoms of node names
-        |> String.split(",")
-        |> Enum.map(&String.to_atom/1)
-        # connect to all nodes to make a cluster
-        |> Enum.each(&Node.connect/1)
-
-      _ ->
-        nil
-    end
+    topologies = [
+      example: [
+        strategy: Elixir.Cluster.Strategy.DNSPoll,
+        config: [query: "tasks.t_ex_cluster_demo", node_basename: "ex_cluster"]
+      ]
+    ]
 
     children = [
+      {Cluster.Supervisor, [topologies, [name: ExCluster.ClusterSupervisor]]},
       {ExCluster.StateHandoff, []},
-      {Horde.Registry, [name: ExCluster.Registry, keys: :unique, members: registry_members()]},
-      {Horde.Supervisor,
-       [
-         name: ExCluster.OrderSupervisor,
-         shutdown: 1000,
-         strategy: :one_for_one,
-         members: supervisor_members()
-       ]},
-      %{
-        id: ExCluster.ClusterConnector,
-        restart: :transient,
-        start:
-          {Task, :start_link,
-           [
-             fn ->
-               Horde.Supervisor.wait_for_quorum(ExCluster.OrderSupervisor, 30_000)
-
-               Enum.each(Node.list(), fn node ->
-                 :ok = ExCluster.StateHandoff.join(node)
-               end)
-             end
-           ]}
-      }
+      {Horde.Registry, [name: ExCluster.HordeRegistry, keys: :unique]},
+      {Horde.Supervisor, [name: ExCluster.HordeSupervisor, shutdown: 1000, strategy: :one_for_one]},
+      {ExCluster.ClusterConnector, restart: :transient},
+      {ExCluster.OrderStarter, []}
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one, name: ExCluster.Supervisor)
-  end
-
-  defp registry_members do
-    [Node.self() | Node.list()]
-    |> Enum.map(fn node -> {ExCluster.Registry, node} end)
-  end
-
-  defp supervisor_members do
-    [Node.self() | Node.list()]
-    |> Enum.map(fn node -> {ExCluster.OrderSupervisor, node} end)
   end
 end
